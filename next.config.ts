fa8808b9c0201c9e585dc0f754c9cedc592a770d
Donaldo <dkumaraku@ritech.co>
2025-02-28 01:41:31 +0100

    add update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "upload.wikimedia.org",
+        protocol: "https"
+      },
+      {
+        hostname: "i.ytimg.com",
+        protocol: "https"
+      },
+      {
+        hostname: "img.jakpost.net",
+        protocol: "https"
+      },
+      {
+        hostname: "i.vimeocdn.com",
+        protocol: "https"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('authToken' as any)
+
+  if (!token) {
+    const loginUrl = new URL('/login', request.url)
+    return NextResponse.redirect(loginUrl)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/watch/:path*', "/order/:path*"]
+}
